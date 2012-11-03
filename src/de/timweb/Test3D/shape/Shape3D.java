311fad13e1ca7b51d9442e3a1402b77f3f37a3f8
Tim Schmiedl <tim@timweb.de>
2012-11-03 22:52:43 +0100

    add SerialID

--- src/de/timweb/Test3D/shape/Shape3D.java
+++ src/de/timweb/Test3D/shape/Shape3D.java
@@ -1,8 +1,10 @@
 package de.timweb.Test3D.shape;
 
+import java.io.Serializable;
+
 import de.timweb.Test3D.math.Vector3d;
 
-public abstract class Shape3D implements Movable{
+public abstract class Shape3D implements Movable,Serializable{
 
 	@Override
 	public void move(Vector3d move) {
