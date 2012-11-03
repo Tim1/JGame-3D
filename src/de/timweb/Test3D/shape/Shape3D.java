4d855eadc17da3d07f0f2f7942216b64896e882d
Tim Schmiedl <tim@timweb.de>
2012-11-03 22:56:04 +0100

    SerialID

--- src/de/timweb/Test3D/shape/Shape3D.java
+++ src/de/timweb/Test3D/shape/Shape3D.java
@@ -6,6 +6,8 @@ import de.timweb.Test3D.math.Vector3d;
 
 public abstract class Shape3D implements Movable,Serializable{
 
+	private static final long serialVersionUID = 4354793157731582366L;
+
 	@Override
 	public void move(Vector3d move) {
 		move(move.x, move.y, move.z);
